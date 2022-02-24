b49782e513572c515ff0cad6bacfcedd83de24c3
anandarpit <orionarpit@gmail.com>
2022-02-24 23:50:03 +0530

    small

--- src/config/db.js
+++ src/config/db.js
@@ -9,20 +9,20 @@ const pool = new Pool({
 })
 
 
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error(
-            'Error acquiring client', err.stack)
-    }
-    client.query('SELECT NOW()', (err, result) => {
-        release()
-        if (err) {
-            return console.error(
-                'Error executing query', err.stack)
-        }
-        console.log("Connected to Database !")
-    })
-})
+// pool.connect((err, client, release) => {
+//     if (err) {
+//         return console.error(
+//             'Error acquiring client', err.stack)
+//     }
+//     client.query('SELECT NOW()', (err, result) => {
+//         release()
+//         if (err) {
+//             return console.error(
+//                 'Error executing query', err.stack)
+//         }
+//         console.log("Connected to Database !")
+//     })
+// })
 
 
 module.exports = pool;
